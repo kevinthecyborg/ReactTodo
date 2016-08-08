3cba256fedf3beed4f234408e4fa575fef233cda
cyborg <kevinthecyborg@Kevins-MacBook-Pro.local>
2016-08-07 23:03:01 -0700

    first commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            ReactBoilerplate
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>
+              Home
+            </IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Created By <a href="http://cyborg.ai" target="_blank">Kevin Michael</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Nav;
